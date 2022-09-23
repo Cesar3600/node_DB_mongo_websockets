fbcdbac006ba7fc899861f5abe04850ca4d550ce
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2022-09-22 22:44:08 -0500

    v7 - creando server y como vienen las peticiones

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('working')
+const express = require('express')
+
+const app = express()
+const Router = express.Router()
+const PORT = 4000
+app.use(Router)
+
+Router.get('/', (request, response) => {
+	response.send('mensaje de accion!')
+})
+
+app.listen(PORT, () => {
+	console.log(`working on!${PORT}`)
+})
